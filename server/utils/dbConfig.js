d5da155dea753e3f7da5c7bfc549ee7950ccc4b6
Deneth Pinsara <it21158186@my.sliit.lk>
2025-06-28 14:22:07 +0530

    chore(init): setup server side

---
+++ server/utils/dbConfig.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const dbConfig = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL).then(()=>{
+            console.log('✅ Database Connected !!');
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
